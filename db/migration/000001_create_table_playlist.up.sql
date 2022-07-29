7f77a55f1ac2e7406d8e51622531c50b33941b9d
Weslley Ribeiro da Silva <weslleypk422@hotmail.com>
2022-07-29 03:07:47 -0300

    first commit

---
+++ db/migration/000001_create_table_playlist.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS "playlist" (
+    "id" serial PRIMARY KEY NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE IF NOT EXISTS "song" (
+    "title" VARCHAR NOT NULL PRIMARY KEY,
+    "genre" VARCHAR NOT NULL,
+    "artist" VARCHAR NOT NULL,
+    "duration" int NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "queue" (
+    "id_playlist" INT NOT NULL,
+    "title_song" VARCHAR NOT NULL,
+    "position" int NOT NULL,
+    CONSTRAINT "fk_id" FOREIGN KEY ("id_playlist") REFERENCES "playlist" ("id"),
+    CONSTRAINT "fk_title" FOREIGN KEY ("title_song") REFERENCES "song" ("title")
+);
